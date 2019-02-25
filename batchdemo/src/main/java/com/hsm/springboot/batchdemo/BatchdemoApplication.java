b2a61b3e57aca3f6b9769e22c848b1f3b44b8f4e
chetans8 <32525133+chetans8@users.noreply.github.com>
2019-02-25 00:56:07 -0500

    Updating Batch API Gateway and Eureka Server

--- batchdemo/src/main/java/com/hsm/springboot/batchdemo/BatchdemoApplication.java
+++ batchdemo/src/main/java/com/hsm/springboot/batchdemo/BatchdemoApplication.java
@@ -5,9 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableBatchProcessing
+@EnableEurekaClient
 public class BatchdemoApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(BatchdemoApplication.class);
