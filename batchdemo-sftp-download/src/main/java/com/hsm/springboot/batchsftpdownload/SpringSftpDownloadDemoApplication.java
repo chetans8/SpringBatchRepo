8af8d4544bcbd0063aa08e1a65b8c3b78e9ddf96
chetans8 <32525133+chetans8@users.noreply.github.com>
2019-03-18 01:44:08 -0400

    Updated Spring boot SFTP download project

---
+++ batchdemo-sftp-download/src/main/java/com/hsm/springboot/batchsftpdownload/SpringSftpDownloadDemoApplication.java
@@ -0,0 +1,16 @@
+package com.hsm.springboot.batchsftpdownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.config.EnableIntegration;
+
+@SpringBootApplication
+@IntegrationComponentScan
+@EnableIntegration
+public class SpringSftpDownloadDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSftpDownloadDemoApplication.class, args);
+    }
+}
