a0b0ffc7d5ce1f872491a3fbec3c7a8eb2bda4f5
chetans8 <32525133+chetans8@users.noreply.github.com>
2019-04-24 22:31:49 -0400

    Checkin Auth server for JWT token

---
+++ batchdemo-authorization-server/src/main/java/com/hsm/sprinboot/batchdemo/authcodeserver/AuthCodeServerApplication.java
@@ -0,0 +1,11 @@
+package com.hsm.sprinboot.batchdemo.authcodeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthCodeServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AuthCodeServerApplication.class, args);
+	}
+}
